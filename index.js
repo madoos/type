bead1ec54623565099e9d44231d2d70bbba6d5a9
madoos <maurice.ronet.dominguez@gmail.com>
2016-12-11 19:56:53 +0100

    added functional extention of type

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict'
 
 const type = require('./lib/core')
+const util = require('./lib/util')
+const extendType = makeFunctions()
 
-module.exports = {
+module.exports = extendType
 
+function makeFunctions () {
+  const typesNames = ['Array', 'Object', 'Date', 'RegExp', 'string', 'number', 'boolean', 'function', 'symbol']
+  return typesNames.reduce(makeTypeFunction, {})
 }
+
+function makeTypeFunction (container, typeName) {
+  const fnName = `is${util.toUpercase(typeName)}`
+  container[fnName] = util.makeComparator.bind(null, type, typeName)
+  return container
+}
+
